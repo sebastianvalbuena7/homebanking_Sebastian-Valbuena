63fbfa5ec01a4d254bd32cf238671f4afe271a0f
Sebastian Valbuena <sebastianvalbuena25@gmail.com>
2023-01-27 20:27:57 -0500

    Homebanking Terminado y Deploy

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
-FROM gradle:7.6.0-jdk19-alpine
+FROM gradle:7.5.1-jdk11-alpine
 
 COPY . .
 
 RUN gradle build
 
 EXPOSE 8080
+
+ENTRYPOINT ["java","-jar","build/libs/homebanking-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -21,38 +21,38 @@ public class HomebankingApplication {
 		SpringApplication.run(HomebankingApplication.class, args);
 	}
 
-//	public double calculatePercentage(String loanName) {
-//		double percentageLoan = 0;
-//		if(loanName.equals("Personal")) {
-//			percentageLoan = 1.20;
-//		} else if(loanName.equals("Mortgage")) {
-//			percentageLoan = 1.30;
-//		} else if(loanName.equals("Automotive")) {
-//			percentageLoan = 1.40;
-//		} else {
-//			percentageLoan = 1.50;
-//		}
-//		return percentageLoan;
-//	}
-//
-//	@Autowired
-//	private PasswordEncoder passwordEncoder;
-//
-//	@Bean
-//	public CommandLineRunner initData(ClientRepository clientrepository, AccountRepository accountRepository, TransactionRepository transactionRepository, LoanRepository loanRepository, ClientLoanRepository clientLoanRepository, CardRepository cardRepository) {
-//		return (args) -> {
-//			Client melba = new Client("Melba", "Lorenzo", passwordEncoder.encode("password123"), "melbalorenzo@mindhub.com");
-//			clientrepository.save(melba);
-//
-//			Client admin = new Client("admin", "admin", passwordEncoder.encode("admin") ,"admin@correo.com");
-//			clientrepository.save(admin);
-//
-//			Loan loanMortgage = new Loan("Mortgage", 500000, List.of(12, 24, 36, 48, 60),calculatePercentage("Mortgage"));
-//			loanRepository.save(loanMortgage);
-//			Loan loanPersonal = new Loan("Personal", 100000, List.of(6, 12, 24), calculatePercentage("Personal"));
-//			loanRepository.save(loanPersonal);
-//			Loan loanAutomotive = new Loan("Automotive", 300000, List.of(6, 12, 24, 36), calculatePercentage("Automotive"));
-//			loanRepository.save(loanAutomotive);
-//		};
-//	}
+	public double calculatePercentage(String loanName) {
+		double percentageLoan = 0;
+		if(loanName.equals("Personal")) {
+			percentageLoan = 1.20;
+		} else if(loanName.equals("Mortgage")) {
+			percentageLoan = 1.30;
+		} else if(loanName.equals("Automotive")) {
+			percentageLoan = 1.40;
+		} else {
+			percentageLoan = 1.50;
+		}
+		return percentageLoan;
+	}
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Bean
+	public CommandLineRunner initData(ClientRepository clientrepository, AccountRepository accountRepository, TransactionRepository transactionRepository, LoanRepository loanRepository, ClientLoanRepository clientLoanRepository, CardRepository cardRepository) {
+		return (args) -> {
+			Client melba = new Client("Melba", "Lorenzo", passwordEncoder.encode("password123"), "melbalorenzo@mindhub.com");
+			clientrepository.save(melba);
+
+			Client admin = new Client("admin", "admin", passwordEncoder.encode("admin") ,"admin@correo.com");
+			clientrepository.save(admin);
+
+			Loan loanMortgage = new Loan("Mortgage", 500000, List.of(12, 24, 36, 48, 60),calculatePercentage("Mortgage"));
+			loanRepository.save(loanMortgage);
+			Loan loanPersonal = new Loan("Personal", 100000, List.of(6, 12, 24), calculatePercentage("Personal"));
+			loanRepository.save(loanPersonal);
+			Loan loanAutomotive = new Loan("Automotive", 300000, List.of(6, 12, 24, 36), calculatePercentage("Automotive"));
+			loanRepository.save(loanAutomotive);
+		};
+	}
 }
